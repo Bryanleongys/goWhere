17b2d60047f3aa1e45b3c98806063f8f96f4a416
Bryanleongys <bryanleongys@gmail.com>
2021-06-16 02:32:42 +0800

    create account & login debug

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -50,31 +50,24 @@ router.post("/", async (req, res) => {
   res.send(user);
 });
 
-// Register new user
 router.post("/register", async (req, res) => {
-  let user = new User({
-    // name: req.body.name,
-    // email: req.body.email,
-    // passwordHash: bcrypt.hashSync(req.body.password, 10),
-    // phone: req.body.phone,
-    // isAdmin: req.body.isAdmin,
-    // street: req.body.street,
-    // apartment: req.body.apartment,
-    // zip: req.body.zip,
-    // city: req.body.city,
-    // country: req.body.country,
-    name: req.body.name,
-    email: req.body.email,
-    passwordHash: bcrypt.hashSync(req.body.password, 10),
-    isAdmin: req.body.isAdmin,
-  });
-  user = await user.save();
+  let user = await User.findOne({ email: req.body.email });
+  if (user) {
+    return res.status(400).send("The user already exists!");
+  } else {
+    user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      passwordHash: bcrypt.hashSync(req.body.password, 10),
+      isAdmin: req.body.isAdmin,
+    });
+    user = await user.save();
+    if (!user) {
+      return res.status(404).send("the user cannot be created!");
+    }
 
-  if (!user) {
-    return res.status(404).send("the user cannot be created!");
+    res.send(user);
   }
-
-  res.send(user);
 });
 
 router.post("/login", async (req, res) => {
@@ -82,7 +75,7 @@ router.post("/login", async (req, res) => {
   const secret = process.env.secret;
 
   if (!user) {
-    return res.status(400).send("The user not found");
+    return res.status(500).send("The user not found");
   }
 
   if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
